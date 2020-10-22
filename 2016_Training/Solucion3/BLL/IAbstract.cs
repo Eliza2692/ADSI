3aeecb6b5189ec3c1567d19d1f4b17bea8af84c1
Elizabeth Alarcon <eliza2692@hotmail.com>
2020-10-21 21:25:17 -0400

    Version Update

---
+++ 2016_Training/Solucion3/BLL/IAbstract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    interface IAbstract <T> where T:class
+    {
+
+        void Add(T obj);
+
+        void Update(T obj);
+
+        void Delete(Object id);
+
+        List<T> GetAll();
+
+        T GetById(Object id);
+
+    }
+}
